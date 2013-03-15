946b9fdf34e9b7e50e3898a4e5b68b10b557f2b1
fa-weberad <adam_weber@fa.org>
2013-03-15 14:55:46 -0400

    Created empty classes for main() in client and server packages.

---
+++ src/client/Tronmageddon.java
@@ -0,0 +1,16 @@
+package client;
+
+public class Tronmageddon {
+
+	/**
+	 * Execution point for client-side app.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO do some stuff here
+		// (who knows what)
+
+	}
+
+}
---
+++ src/server/TronmagServer.java
@@ -0,0 +1,16 @@
+package server;
+
+public class TronmagServer {
+
+	/**
+	 * Execution point for server side.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// an infinite loop that spawns worker
+		// threads to handle individual client
+		// connections probably needs to be here
+	}
+
+}
