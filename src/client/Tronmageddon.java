adebec8483e78c3902187142a393bb2cd869f87e
Ethan Nyein <nyein6795@aol.com>
2013-03-21 13:26:53 -0400

    Made grid full size

--- src/client/Tronmageddon.java
+++ src/client/Tronmageddon.java
@@ -3,8 +3,9 @@ package client;
 import java.awt.Color;
 
 import info.gridworld.actor.ActorWorld;
-import info.gridworld.actor.Bug;
-import info.gridworld.grid.Location;
+import info.gridworld.actor.*;
+import info.gridworld.grid.*;
+
 
 public class Tronmageddon {
 
@@ -15,10 +16,16 @@ public class Tronmageddon {
 	 */
 	public static void main(String[] args) {
 		ActorWorld world = new ActorWorld();
+		
 		Bug b = new Bug();
+		world.setGrid(new BoundedGrid<Actor>(42,95));
 		world.add(b);
+		
+		
 		world.show();
-
 	}
-
+	
+	public Location getLocation(Actor a){
+		return a.getLocation();
+	}
 }
